3a2628839ce54a490495e970b4bb108714cd1e9a
Keylane Silva <keylanessilva07@gmail.com>
2020-04-17 16:36:45 -0300

    /

---
+++ app/javascript/routes.js
@@ -0,0 +1,26 @@
+// Importantando o React
+import React from "react";
+
+
+// Importantando o component Home
+import HomeScreen from "./screens/home";
+import DiscoveryScreen from "./screens/discovery";
+import SearchScreen from "./screens/search";
+import AlbumScreen from "./screens/album";
+import FavoritesScreen from "./screens/favorite";
+
+
+// Importanto o component <Switch /> e <Route /> da nossa Lib de rotas
+import { Switch, Route } from 'react-router-dom'
+ 
+const Routes = () => (
+  <Switch>
+    <Route exact path='/' component={HomeScreen}/>
+    <Route exact path='/discovery' component={DiscoveryScreen} />
+    <Route exact path='/search' component={SearchScreen} />
+    <Route exact path='/album/:id' component={AlbumScreen} />
+    <Route exact path='/favorites' component={FavoritesScreen} />
+  </Switch>
+);
+ 
+export default Routes;
