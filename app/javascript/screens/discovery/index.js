c6b1412ef999520dd02d86880eab7cedea1d9f88
Keylane Silva <keylanessilva07@gmail.com>
2020-04-17 21:06:57 -0300

    /

--- app/javascript/screens/discovery/index.js
+++ app/javascript/screens/discovery/index.js
@@ -1,12 +1,16 @@
-import React, { Fragment, useState } from 'react';
+import React, {Fragment} from 'react';
+import SectionWrapper from '../../components/common/section_wrapper'
+import Discovery from '../../components/discovery';
 import NavbarFooter from '../../components/common/navbar_footer';
-        
-        const DiscoveryScreen = () => {
-          return(
-            <Fragment>
-              <div>Discovery Screen</div>
-              <NavbarFooter/>
-            </Fragment>
-          );
-        }
-        export default DiscoveryScreen;
+ 
+const DiscoveryScreen = () => {
+  return(
+    <Fragment>
+      <SectionWrapper>
+        <Discovery />
+      </SectionWrapper>
+      <NavbarFooter />
+    </Fragment>
+  );
+}
+export default DiscoveryScreen;
