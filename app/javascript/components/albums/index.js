505bbc939d6a50be4d2066b1c1ee75c88d8e36c5
Keylane Silva <keylanessilva07@gmail.com>
2020-04-18 18:47:39 -0300

    conexao album back

--- app/javascript/components/albums/index.js
+++ app/javascript/components/albums/index.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { Heading, Columns, Image } from 'react-bulma-components';
 import styled from 'styled-components';
+import AlbumsService from '../../services/albums';
+import { useParams } from 'react-router-dom';
 
 const DivVSpaced = styled.div`
      margin-top: 20px;
@@ -8,19 +10,32 @@ const DivVSpaced = styled.div`
 `
 
 const Albums = () => {
+  const [album, setAlbum] = useState({});
+  let { id } = useParams();
 
- return (
-   <Fragment>
-     <Columns className='is-vcentered is-mobile is-centered'>
-       <Columns.Column desktop={{size: 3}} className='has-text-centered'>
-         <Image src=''/>
-         <DivVSpaced>
-           <Heading size={5} className='has-text-white'>Título</Heading>
-           <Heading size={6} subtitle className='has-text-white'>SubTítulo</Heading>
-         </DivVSpaced>
-       </Columns.Column>
-     </Columns>
-   </Fragment>
-);
+  async function fetchAlbum() {
+    const response = await AlbumsService.show(id);
+    setAlbum(response.data);
+  }
+ 
+  useEffect(() => {
+    fetchAlbum();
+  }, []);
+ 
+  
+
+  return (
+    <Fragment>
+      <Columns className='is-vcentered is-mobile is-centered'>
+        <Columns.Column desktop={{size: 3}} className='has-text-centered'>
+          <Image src={album.cover_url}/>
+          <DivVSpaced>
+            <Heading size={5} className='has-text-white'>{album.title}</Heading>
+            <Heading size={6} subtitle className='has-text-white'>{album.artist_name}</Heading>
+          </DivVSpaced>
+        </Columns.Column>
+      </Columns>
+    </Fragment>
+  );
 }
 export default Albums;
