a5121c1af0f2f1ca0c9c7acee158cd87de3dc528
Keylane Silva <keylanessilva07@gmail.com>
2020-04-19 00:19:26 -0300

    /

--- app/javascript/components/search/search_bar/index.js
+++ app/javascript/components/search/search_bar/index.js
@@ -1,10 +1,28 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useState } from 'react';
+import { Form } from 'react-bulma-components';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+   const [query, setQuery] = useState("");
+
+   const Search = (e) => {
+      if (e.key === 'Enter') {
+         props.fetchSearch(query);
+         setQuery("")
+       }
+     }
 
  return (
    <Fragment>
-​
+     <Form.Field onKeyDown={Search}>
+       <Form.Control iconRight>
+         <Form.Input
+           placeholder="Text input"
+           placeholder='Álbuns, artistas ou músicas'
+           value={query}
+           onChange={e => setQuery(e.target.value)}   
+/>
+       </Form.Control>
+     </Form.Field>
    </Fragment>
 );
 }
--- app/javascript/services/categories.js
+++ app/javascript/services/categories.js
@@ -1,7 +1,8 @@
 import Api from './api';
 
 const CategoriesService = {
- show: (id) => Api.get(`/categories/${id}`)
+ show: (id) => Api.get(`/categories/${id}`),
+ index: () => Api.get('/categories')
 }
 
 export default CategoriesService;
