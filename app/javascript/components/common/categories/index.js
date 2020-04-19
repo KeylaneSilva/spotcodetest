5c1f4bc0aa96b24fb73b9bc37414f2bff950b3ff
Keylane Silva <keylanessilva07@gmail.com>
2020-04-18 22:58:03 -0300

    /

---
+++ app/javascript/components/common/categories/index.js
@@ -0,0 +1,10 @@
+import React, { Fragment, useEffect, useState } from 'react';
+
+const Categories = () => {
+ return (
+   <Fragment>
+​
+   </Fragment>
+);
+}
+export default Categories;
---
+++ app/javascript/components/search/search_bar/index.js
@@ -0,0 +1,11 @@
+import React, { Fragment, useEffect, useState } from 'react';
+
+const SearchBar = () => {
+
+ return (
+   <Fragment>
+​
+   </Fragment>
+);
+}
+export default SearchBar;
